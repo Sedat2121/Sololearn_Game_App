c68a588941329a19f1cf726ca3f897ca9b08de21
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-21 13:32:42 +0000

    canvas is added

--- code.js
+++ code.js
@@ -6,3 +6,13 @@ window.onload = function() {
         count += 1;
     }
 } 
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var x = 300;
+var y = 350;
+
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.fillStyle="red";
+    context.fill(); 
+
