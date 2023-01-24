1b60e5f568c8f1d9fef1521d7be4129997bdc2ab
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-24 11:35:59 +0000

    onload function added with settime Interval

--- code.js
+++ code.js
@@ -1,28 +1,52 @@
-window.onload = function () {
-    let btn = document.getElementById("jump");
-    let count = 0;
-
-    btn.onclick = function () {
-        count += 1;
-    }
-}
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var x = 300;
 var y = 350;
-
 context.arc(x, y, 50, 0, 2 * Math.PI);
 context.fillStyle = "red";
 context.fill();
 
-function draw() {
-    context.beginPath();
-    context.rect(x, y, 100, 100);
-    context.fillStyle = "red";
-    context.fill();
-    x += 10;
-    if (x >= 600) {
-        x = -100
+window.onload = function() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var x = 0;
+    var y = 100;
+    
+    function draw() {
+        context.beginPath();
+        context.rect(x, y, 100, 100);
+        context.fillStyle="red";
+        context.fill();
+
+        x += 10;
+        if (x >= 600) {
+            x = -100;
+        }
     }
+    setInterval(draw, 50);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onload = function () {
+//     let btn = document.getElementById("jump");
+//     let count = 0;
+
+//     btn.onclick = function () {
+//         count += 1;
+//     }
+// }
+
+
+
