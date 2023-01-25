be23d8cb5b5ad671113abf030f4345053c32a310
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-25 20:10:22 +0000

    Frame Rate notes and functions added

--- code.js
+++ code.js
@@ -13,20 +13,26 @@ window.onload = function() {
     var x = 0;
     var y = 100;
     
-    var t;
+    var t = Date.now();
+    let speed = 100;
     function draw() {
         var timePassed = (Date.now() - t) / 1000;
         t = Date.now();
         var fps = Math.round(1 / timePassed);
+
         context.clearRect(0, 0, 600, 400);
+        
         context.font = '25px Arial';
         context.fillStyle = 'black';
         context.fillText("FPS: " + fps, 20, 30);
+
         context.beginPath();
         context.rect(x, y, 100, 100);
         context.fillStyle="red";
         context.fill();
-        x += 10;
+
+        x += (speed * timePassed); 
+
         if (x >= 600) {
             x = -100;
         }
